e23e805950a7fb83fed5b3b00abf9337b31dfe3a
eddiejpot <jamesedwardporter@gmail.com>
2021-09-13 23:01:46 +0800

    connected code editor and diagram

--- src/components/DiagramAndCodeEditor/DiagramAndCodeEditor.jsx
+++ src/components/DiagramAndCodeEditor/DiagramAndCodeEditor.jsx
@@ -1,22 +1,52 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import Diagram from "../Diagram/Diagram.jsx"
 import CodeEditor from "../CodeEditor/CodeEditor.jsx";
 import { dbmlToGoJs } from "../../utils/parser/dbmlToGoJsObject.js";
-import {initialStateDbml} from "../Diagram/diagramInitialStateDbml.js"
+import diagramDataInitialStateInDbml from "../Diagram/diagramInitialStateDbml.js"
+// import diagramDataInitialStateInGoJs from "../Diagram/diagramInitialStateGoJsObj.js"
 import SplitPane from "react-split-pane";
 import './style.css';
 
-const result = dbmlToGoJs(initialStateDbml)
-
 //========================================== MAIN COMPONENT
 export default function DiagramAndCodeEditor() {
+  // Manage State
+  const [diagramDbmlData, setDiagramDbmlData] = useState(diagramDataInitialStateInDbml);
+  const [diagramGoJsData, setDiagramGoJsData] = useState();
+  const isDataForDiagramReady = useRef(false);
+  const lastDbmlDataWithoutErrors = useRef(diagramDataInitialStateInDbml);
 
-  const [renderDiagram, setRenderDiagram] = useState(true);
 
-  const ComponentToRender = () =>{
-    if (renderDiagram){
+
+
+  useEffect(() => {
+    // get updated code from codeeditor component in dbml format
+    if (diagramDbmlData.length !== 0){
+      console.log('RENDERING ==========>')
+      let parseDbmlToGoJs;
+      if (dbmlToGoJs(diagramDbmlData) === 'error'){
+        console.log('ERROR IN PARSING ==========>')
+        parseDbmlToGoJs = dbmlToGoJs(lastDbmlDataWithoutErrors.current);
+      } else {
+        console.log('DI NOT DETECT ERROR IN PARSING ==========>')
+        lastDbmlDataWithoutErrors.current = diagramDbmlData;
+        parseDbmlToGoJs = dbmlToGoJs(diagramDbmlData);
+      }
+      // update state
+      isDataForDiagramReady.current = true;
+      setDiagramGoJsData(()=>parseDbmlToGoJs);
+
+    } else {
+      console.log('NO GONNA RENDER');
+      isDataForDiagramReady.current = false;
+    }
+
+  },[diagramDbmlData]);
+
+
+  const DiagramToRender = () =>{
+    if (isDataForDiagramReady.current){
       return(
-        <Diagram diagramData = {result}/>
+        <Diagram diagramData = {diagramGoJsData}/>
       )
     }
     return <h1>Loading</h1>
@@ -26,8 +56,8 @@ export default function DiagramAndCodeEditor() {
     <div className="App">
       {/* <button onClick={()=> setRenderDiagram(!renderDiagram)}>CLICK ME</button> */}
       <SplitPane split="vertical" minSize={50} defaultSize={450}>
-        <CodeEditor/>
-        <ComponentToRender />
+        <CodeEditor diagramDbmlData={diagramDbmlData} setDiagramDbmlData={setDiagramDbmlData}/>
+        <DiagramToRender/>
       </SplitPane>
     </div>
   );
